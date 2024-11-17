fc22c0374888797d0dbe86c9a4c0d468227059e4
Smadar Tsdaka <smadar@irpsystems.com>
2024-11-17 10:06:51 +0200

    Build CSV headers the step and parent step description

--- EOL/Services/CSVWriter.cs
+++ EOL/Services/CSVWriter.cs
@@ -1,28 +1,37 @@
 ﻿using EOL.Models;
-using System;
+using ScriptHandler.Models;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using CsvHelper;
 
 namespace EOL.Services
 {
     public class CSVWriter
     {
-        public string _csvFilePath { get; set; }
+		#region Properties and Fields
+
+		public string _csvFilePath { get; set; }
 
         private List<string> _headers;
         private bool _headersWritten;
 
-        public CSVWriter()
+		#endregion Properties and Fields
+
+		#region Constructor
+
+		public CSVWriter()
         {
             _headers = new List<string>();
             _headersWritten = false;
         }
 
-        public void WriteTestResult(RunResult testResult)
+		#endregion Constructor
+
+		#region Methods
+
+		public void WriteTestResult(RunResult testResult)
         {
             if (string.IsNullOrEmpty(_csvFilePath))
                 return;
@@ -38,10 +47,11 @@ namespace EOL.Services
             // Collect headers if not already done
             if (!_headersWritten)
             {
-                var stepHeaders = testResult.Steps.Select(s => s.StepDescription).ToList();
-                _headers = standardHeaders.Concat(stepHeaders).ToList();
+                _headers = GetHeaders(testResult.Steps);
+				_headers.InsertRange(0, standardHeaders);
 
-                using (var writer = new StreamWriter(_csvFilePath, append: false))
+
+				using (var writer = new StreamWriter(_csvFilePath, append: false))
                 {
                     // Write headers
                     writer.WriteLine(string.Join(",", _headers));
@@ -61,12 +71,37 @@ namespace EOL.Services
 
                 foreach (var header in _headers.Skip(standardHeaders.Count))
                 {
-                    var step = testResult.Steps.FirstOrDefault(s => s.StepDescription == header);
+                    var step = testResult.Steps.FirstOrDefault(s => s.ParentStepDescription == header);
                     rowValues.Add(step?.TestValue.ToString() ?? "");
                 }
 
                 writer.WriteLine(string.Join(",", rowValues));
             }
         }
-    }
+
+        private List<string> GetHeaders(List<EOLStepSummeryData> Steps)
+        {
+            List<string> headers = new List<string>();
+
+            foreach (EOLStepSummeryData step in Steps)
+            {
+                if (step.Step.EOLReportsSelectionData.IsSaveToReport == false)
+                    continue;
+
+                string description = string.Empty;
+
+                if (string.IsNullOrEmpty(step.ParentStepDescription) == false)
+                    description += $"{step.ParentStepDescription} -- ";
+
+                description += step.Description;
+
+                headers.Add(description);
+
+            }
+
+            return headers;
+        }
+
+		#endregion Methods
+	}
 }
--- EOL/ViewModels/RunViewModel.cs
+++ EOL/ViewModels/RunViewModel.cs
@@ -702,61 +702,71 @@ namespace EOL.ViewModels
 		}
 
 		private void Stop(ScriptStopModeEnum stopeMode)
-		{			
-			_timerDuration.Stop();
-			_runData.EndTime = DateTime.Now;
-
-
-			IsRunButtonEnabled = true;
-
-			if (stopeMode == ScriptStopModeEnum.Aborted)
-				RunState = RunStateEnum.Aborted;
-			else
-				RunState = RunStateEnum.Ended;
-
-            List<EOLStepSummeryData> eolStepSummerysList = new List<EOLStepSummeryData>();
-			foreach (GeneratedProjectData project in _generatedProjectsList)
+		{
+			try
 			{
-				foreach (GeneratedScriptData script in project.TestsList)
+				_timerDuration.Stop();
+				_runData.EndTime = DateTime.Now;
+
+
+				IsRunButtonEnabled = true;
+
+				if (stopeMode == ScriptStopModeEnum.Aborted)
+					RunState = RunStateEnum.Aborted;
+				else
+					RunState = RunStateEnum.Ended;
+
+				List<EOLStepSummeryData> eolStepSummerysList = new List<EOLStepSummeryData>();
+				foreach (GeneratedProjectData project in _generatedProjectsList)
 				{
-					GetScriptEOLStepSummerys(
-						script,
-						eolStepSummerysList);
+					foreach (GeneratedScriptData script in project.TestsList)
+					{
+						GetScriptEOLStepSummerys(
+							script,
+							eolStepSummerysList);
+					}
 				}
+
+				int passed;
+				int failed;
+				GetPassFailed(
+					eolStepSummerysList,
+					out passed,
+					out failed);
+
+				_runData.NumberOfTested = passed + failed;
+				_runData.NumberOfFailed = failed;
+				_runData.NumberOfPassed = passed;
+
+				if (failed > 0)
+				{
+					RunState = RunStateEnum.Failed;
+					_singleTestResult.TestStatus = "Failed";
+					_stepSetParameter.Value = 0;
+				}
+				else
+				{
+					_singleTestResult.TestStatus = "Passed";
+					_stepSetParameter.Value = 1;
+				}
+
+				_stepSetParameter.Execute();
+
+				_singleTestResult.SerialNumber = _runData.SerialNumber;
+				_singleTestResult.PartNumber = _runData.PartNumber;
+				_singleTestResult.OperatorName = _runData.OperatorName;
+				_singleTestResult.Steps = eolStepSummerysList;
+				_singleTestResult.StartTimeStamp = _runData.StartTime.ToString("dd-MMM-yyyy hh:mm:ss.fff");
+				_singleTestResult.EndTimeStamp = _runData.EndTime.ToString("dd-MMM-yyyy hh:mm:ss.fff");
+
+				_csvWritter.WriteTestResult(_singleTestResult);
+
+				_pdfCreator.CreatePdf(_generatedProjectsList, _singleTestResult, _userDefaultSettings);
 			}
-
-            int passed;
-            int failed;
-            GetPassFailed(
-                eolStepSummerysList,
-                out passed,
-                out failed);
-
-            _runData.NumberOfTested = passed + failed;
-            _runData.NumberOfFailed = failed;
-            _runData.NumberOfPassed = passed;
-
-			if (failed > 0)
+			catch (Exception ex)
 			{
-				RunState = RunStateEnum.Failed;
-				_singleTestResult.TestStatus = "Failed";
-				_stepSetParameter.Value = 0;
+				LoggerService.Error(this, "Faild to handle stop tasks", ex);
 			}
-			else
-			{
-                _singleTestResult.TestStatus = "Passed";
-                _stepSetParameter.Value = 1;
-            }
-
-            _stepSetParameter.Execute();
-
-            _singleTestResult.SerialNumber = _runData.SerialNumber;
-			_singleTestResult.PartNumber = _runData.PartNumber;
-			_singleTestResult.OperatorName = _runData.OperatorName;
-            _singleTestResult.Steps = eolStepSummerysList;
-            _csvWritter.WriteTestResult(_singleTestResult);
-
-			_pdfCreator.CreatePdf(_generatedProjectsList, _singleTestResult, _userDefaultSettings);
 		}
 
 		private void _timerDuration_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
